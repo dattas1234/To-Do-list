17ad2e3e92330560f6f5ae91bfd0a85b2b817100
Datta soat <76569205+dattas1234@users.noreply.github.com>
2021-08-28 01:41:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import "./styles.css";
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+const tasks = {
+  paddingLeft: "25%",
+  paddingRight: "25%"
+};
+
+const faintRed = "rgb(245,102,66)";
+
+export default function App() {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const onChange = (e) => {
+    setNewTask(e.target.value);
+    // console.log(newTask);
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+
+    setTask((prev) => [...prev, newTask]);
+    // console.log(task);
+  };
+
+  const onDelete = (taskToDelete) => {
+    const newTasks = () => task.filter((task, index) => task !== taskToDelete);
+    setTask(newTasks());
+  };
+  const checkLength = () => {
+    if (task.length === 0)
+      return (
+        <h4
+          style={{
+            backgroundColor: faintRed,
+            display: "inline",
+            padding: "5px 30px",
+            borderRadius: 7,
+            boxShadow: "2px 3px 3px"
+          }}
+        >
+          No tasks to show{" "}
+        </h4>
+      );
+  };
+
+  return (
+    <div className="App">
+      <h1 style={{ color: "#ffffff" }}>Assign your tasks</h1>
+
+      <form onSubmit={addTask}>
+        <div>
+          <lable style={{ color: "rgb(252, 194, 3)" }}>Enter Task </lable>
+        </div>
+        <br />
+        <div>
+          <input
+            className="InputStyles"
+            type="text"
+            onChange={onChange}
+            required
+            autoFocus
+          />
+        </div>
+
+        <br />
+        <div>
+          <input className="ButtonStyles" type="submit" value="ADD" />
+        </div>
+        {/* <div> <button onClick={addTask}>ADD</button></div> */}
+      </form>
+      <h2>Your Tasks</h2>
+      {checkLength()}
+
+      <div style={tasks}>
+        {task.map((task, index) => (
+          <div
+            style={{
+              borderLeft: " solid 4px blue",
+              padding: 4,
+              backgroundColor: "pink",
+              marginBottom: "5px",
+              display: "flex",
+              height: 20,
+              position: "relative",
+              boxShadow: "2px 3px 3px"
+            }}
+          >
+            {/* <h3 style={{display:"inline",}} key={index}>{task}</h3> */}
+            <span
+              style={{ display: "inline", position: "absolute", left: 10 }}
+              key={index}
+            >
+              {task}
+            </span>
+            <span
+              style={{ display: "inline", position: "absolute", right: 10 }}
+            >
+              <FaTrashAlt
+                onClick={() => onDelete(task)}
+                style={{ cursor: "pointer" }}
+              />{" "}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
